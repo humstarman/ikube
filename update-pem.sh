5028774f13174eb52264e140a3a04ded0091c66b
humstarman <lueyoung7@gmail.com>
2018-07-26 13:54:33 +0800

    update

--- update-pem.sh
+++ update-pem.sh
@@ -1,143 +1,161 @@
 #!/bin/bash
-
 set -e
-
-START=$(date +%s)
-WAIT=3
-STAGE=0
-STAGE_FILE=stage.update
-if [ ! -f ./${STAGE_FILE} ]; then
-  INIT=0
-  touch ./${STAGE_FILE}
-  echo 0 > ./${STAGE_FILE} 
-else
-  INIT=1
-fi
-getScript () {
-  URL=$1
-  SCRIPT=$2
-  curl -s -o ./$SCRIPT $URL/$SCRIPT
-  chmod +x ./$SCRIPT
-}
+DEFAULT_YEAR=1
+RUN=false
 show_help () {
 cat << USAGE
-usage: $0
-  - This script is to update the permissions, including:
-  - CA
-  - ETCD
-  - Admin of Kubectl & kubeconfig file of Kubectl
-  - Flanneld
-  - Kubernetes
-  - Kubelet bootstrapping kubeconfig
-  - Kube-proxy & kubeconfig file of kube-proxy
-  -
+usage: $0 [ -r RUN-FLAG ] [ -y YEAR ]
+use to update permissons used in Kubernetes.
+
+    -y : Specify the period of validity in term of year.
+         If not specified, use "$DEFAULT_YEAR" by default.
+    -r : Specify the flag to run. 
+
+This script should run on a Master node.
 USAGE
-if [[ "0" == "$INIT" ]]; then
-  cat << USAGE
-  ---
-  ---
-  ---
-  If you run the script for the first time,
-  the help document shown by default.
-  Re-run the script to function.
-  ---
-USAGE
-fi
-sleep $WAIT
 exit 0
 }
-if [[ "0" == "$INIT" ]]; then
-  show_help
-fi  
 # Get Opts
-while getopts "h" opt; do 
+while getopts "hy:r" opt; do # 选项后面的冒号表示该选项需要参数
     case "$opt" in
     h)  show_help
         ;;
-    ?)
-        echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [ERROR] - unkonw argument."
+    r)  RUN=true 
+        ;;
+    y)  YEAR=$OPTARG
+        ;;
+    ?)  # 当有不认识的选项的时候arg为?
+        echo "unkonw argument"
         exit 1
         ;;
     esac
 done
-PROJECT="test-demo"
-<<<<<<< HEAD
-BRANCH=master
-=======
-BRANCH=master
->>>>>>> v1.11_flannel
-URL=https://raw.githubusercontent.com/humstarman/${PROJECT}-impl/${BRANCH}
-TOOLS=${URL}/tools
-THIS_FILE=$0
-PREFIX=$THIS_FILE
-PREFIX=${PREFIX##*/}
-PREFIX=${PREFIX%.*}
-MAIN=${URL}/${PREFIX}
+[ -z "$*" ] && show_help
+if ! $RUN; then
+  echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [ERROR] - set the -r flag to run."
+  exit 1 
+fi
+YEAR=${YEAR:-"${DEFAULT_YEAR}"}
+STAGE=0
+STAGE_FILE=stage.update
+if [ ! -f ./${STAGE_FILE} ]; then
+  touch ./${STAGE_FILE}
+  echo 0 > ./${STAGE_FILE}
+fi
+getScript () {
+  TRY=10
+  URL=$1
+  SCRIPT=$2
+  for i in $(seq -s " " 1 ${TRY}); do
+    curl -s -o ./$SCRIPT $URL/$SCRIPT
+    if cat ./$SCRIPT | grep "404: Not Found"; then
+      rm -f ./$SCRIPT
+    else
+      break
+    fi
+  done
+  if [ -f "./$SCRIPT" ]; then
+    chmod +x ./$SCRIPT
+  else
+    echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [ERROR] - downloading failed !!!" 
+    echo " - $URL/$SCRIPT"
+    echo " - Please check !!!"
+    sleep 3
+    exit 1
+  fi
+}
+# restore from backup
+BIN=restore-from-backup.sh
+cat > ./$BIN<<"EOF"
+#!/bin/bash
+set -e
+# 0 set env 
+BAK_DIR=/var/k8s/bak
+getBackup () {
+  BAK_DIR=${1:-"/var/k8s/bak"}
+  yes | cp -r $BAK_DIR/* ./
+}
+# 1 restore from backup
+if [ -d "$BAK_DIR" ]; then
+  getBackup $BAK_DIR
+else
+  echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [ERROR] - no $BAK_DIR found !!!"
+  sleep 3
+  exit 1
+fi
+exit 0
+EOF
+chmod +x ./$BIN
+./$BIN
+source ./info.env
 
 # 0 clear expired permission & check cfssl tool
 if [[ "$(cat ./${STAGE_FILE})" == "0" ]]; then
-  curl -s $TOOLS/check-k8s-cluster.sh | /bin/bash 
-  curl -s $TOOLS/restore-info-from-backup.sh | /bin/bash
-  curl -s $TOOLS/check-needed-files.sh | /bin/bash
-  curl -s $TOOLS/check-ansible.sh | /bin/bash 
-  getScript $MAIN clear-expired-pem.sh
-  ansible all -m script -a ./clear-expired-pem.sh
-  curl -s $MAIN/check-cfssl.sh | /bin/bash 
+  curl -s $SCRIPTS/check-k8s-cluster.sh | /bin/bash 
+  curl -s $SCRIPTS/check-needed-files.sh | /bin/bash
+  curl -s $SCRIPTS/check-ansible.sh | /bin/bash 
+  curl -s $SCRIPTS/mk-ansible-available.sh | /bin/bash 
+  getScript $STAGES clear-expired-pem.sh
+  ansible ${ANSIBLE_GROUP} -m script -a ./clear-expired-pem.sh
+  curl -s $STAGES/check-cfssl.sh | /bin/bash 
 fi
 
 # 1 CA
 STAGE=$[${STAGE}+1]
 if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
-  curl -s $MAIN/update-ca-pem.sh | /bin/bash 
+  getScript $STAGES update-ca-pem.sh
+  ./update-ca.pem.sh -y $YEAR -g ${ANSIBLE_GROUP}
   echo $STAGE > ./${STAGE_FILE}
 fi
 
 # 2 etcd 
 STAGE=$[${STAGE}+1]
 if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
-  curl -s $MAIN/update-etcd-pem.sh | /bin/bash 
+  curl -s $STAGES/update-etcd-pem.sh | /bin/bash 
   echo $STAGE > ./${STAGE_FILE}
 fi
 
 # 3 kubectl 
 STAGE=$[${STAGE}+1]
 if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
-  curl -s $MAIN/update-kubectl-pem.sh | /bin/bash 
+  curl -s $STAGES/update-kubectl-pem.sh | /bin/bash 
   echo $STAGE > ./${STAGE_FILE}
 fi
 
 # 4 flanneld 
 STAGE=$[${STAGE}+1]
 if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
-  curl -s $MAIN/update-flanneld-pem.sh | /bin/bash 
+  if [[ "flannel" == "${CNI}" ]]; then
+    curl -s $STAGES/update-flanneld-pem.sh | /bin/bash 
+  fi
   echo $STAGE > ./${STAGE_FILE}
 fi
 
 # 5 kubernetes 
 STAGE=$[${STAGE}+1]
 if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
-  curl -s $MAIN/update-kubernetes-pem.sh | /bin/bash 
+  curl -s $STAGES/update-kubernetes-pem.sh | /bin/bash 
   echo $STAGE > ./${STAGE_FILE}
 fi
 
 # 6 kubelet 
 STAGE=$[${STAGE}+1]
 if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
-  curl -s $MAIN/update-kubelet-pem.sh | /bin/bash 
+  curl -s $STAGES/update-kubelet-pem.sh | /bin/bash 
   echo $STAGE > ./${STAGE_FILE}
 fi
 
 # 7 kube-proxy 
 STAGE=$[${STAGE}+1]
 if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
-  curl -s $MAIN/update-kube-proxy-pem.sh | /bin/bash 
+  curl -s $STAGES/update-kube-proxy-pem.sh | /bin/bash 
   echo $STAGE > ./${STAGE_FILE}
 fi
 
 # 8 restart services 
 STAGE=$[${STAGE}+1]
 if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
-  curl -s $MAIN/restart-svc.sh | /bin/bash 
+  curl -s $STAGES/restart-svc.sh | /bin/bash 
   echo $STAGE > ./${STAGE_FILE}
 fi
 
@@ -145,21 +163,21 @@ fi
 STAGE=$[${STAGE}+1]
 if [[ "$(cat ./${STAGE_FILE})" < "$STAGE" ]]; then
   echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - Kubernetes permmisson has been updated. "
-  curl -s $TOOLS/clearance.sh | /bin/bash 
+  curl -s $SCRIPTS/clearance.sh | /bin/bash 
   echo $STAGE > ./${STAGE_FILE}
 fi
 
 # ending
-echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - As updating permmisons, re-approving certificate is needed."
+echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - As updating permissons, re-approving certificate is needed."
 echo " - sleep $WAIT sec, then apporve."
 FILE=approve-pem.sh
 if [ ! -f "$FILE" ]; then
-  cat > $FILE << EOF
+  cat > $FILE <<"EOF"
 #!/bin/bash
-CSRS=\$(kubectl get csr | grep Pending | awk -F ' ' '{print \$1}')
-if [ -n "\$CSRS" ]; then
-  for CSR in \$CSRS; do
-    kubectl certificate approve \$CSR
+CSRS=$(kubectl get csr | grep Pending | awk -F ' ' '{print $1}')
+if [ -n "$CSRS" ]; then
+  for CSR in $CSRS; do
+    kubectl certificate approve $CSR
   done
 fi
 EOF
