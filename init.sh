ce718e450c9669452a53cc6be28240bcad578fb1
humstarman <lueyoung7@gmail.com>
2018-08-16 22:35:59 +0800

    update

--- init.sh
+++ init.sh
@@ -318,5 +318,15 @@ echo " - With masters: $N_MASTER"
 THIS_DIR=$(cd "$(dirname "$0")";pwd)
 curl -s $SCRIPTS/mk-backup.sh | /bin/bash
 echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [INFO] - backup important info from $THIS_DIR to /var/k8s/bak."
+FILE=approve-pem.sh
+cat > $FILE <<"EOF"
+#!/bin/bash
+CSRS=$(kubectl get csr | grep Pending | awk -F ' ' '{print $1}')
+if [ -n "$CSRS" ]; then
+  for CSR in $CSRS; do
+    kubectl certificate approve $CSR
+  done
+fi
+EOF
 sleep $WAIT 
 exit 0
